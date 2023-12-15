c707c837fe4eceb0b0b9eff0bd3a40b45c531396
Raquel Gabrielli Ribeiro Lopes <raquel.lopes@corp.pmaracoiba.local>
2023-12-15 16:24:41 -0300

    Projeto RocketSeat 1

---
+++ todolist/src/main/java/br/com/raquellopes/todolist/user/UserController.java
@@ -0,0 +1,18 @@
+package br.com.raquellopes.todolist.user;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
+
